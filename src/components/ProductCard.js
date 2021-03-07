1d771e10ae4f30986f2fd373f8fe38c396344b4c
Gaurav Kumar <gauravk26800@gmail.com>
2021-03-07 17:38:50 +0530

    fixed netlify redirect added more values in product data

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style/productCard.css";
 
 const ProductCard = ({ item, setCartItems, cartItems }) => {
   const addItemToCart = () => {
-    setCartItems([...cartItems, item]);
-    console.log("item", item);
+    item.addedToCart = true;
+    item.addToCartButtonClass = "btn btn-danger";
+    item.addToCartButtonValue = "Remove from Cart";
   };
+
+  const removeItemFromCart = () => {
+    item.addedToCart = false;
+    item.addToCartButtonClass = "btn btn-info";
+    item.addToCartButtonValue = "Add to Cart";
+  };
+
+  // useEffect(() => {
+  //   addItemToCart();
+  // }, [item]);
+
+  // useEffect(() => {
+  //   removeItemFromCart();
+  // }, [item]);
 
   return (
     <div className="product-card col-lg-3 col-md-4 col-sm-6">
@@ -23,10 +38,10 @@ const ProductCard = ({ item, setCartItems, cartItems }) => {
           <li className="list-group-item">Quantity</li>
         </ul>
         <div className="card-body">
-          <button className="btn btn-info" onClick={addItemToCart}>
-            Add to Cart
+          <button className={item.addToCartButtonClass} onClick={addItemToCart}>
+            {item.addToCartButtonValue}
           </button>
-          <button className="btn btn-primary" onClick={addItemToCart}>
+          <button className="btn btn-primary" onClick={removeItemFromCart}>
             Order Now
           </button>
         </div>
--- src/components/data/products.js
+++ src/components/data/products.js
@@ -6,6 +6,9 @@ const products = [
     description:
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
     category: "men clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
   },
   {
@@ -15,6 +18,9 @@ const products = [
     description:
       "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
     category: "men clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image:
       "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
   },
@@ -25,6 +31,9 @@ const products = [
     description:
       "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
     category: "men clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
   },
   {
@@ -34,6 +43,9 @@ const products = [
     description:
       "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
     category: "men clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
   },
   {
@@ -44,6 +56,9 @@ const products = [
     description:
       "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
     category: "jewelery",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
   },
   {
@@ -53,6 +68,9 @@ const products = [
     description:
       "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
     category: "jewelery",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
   },
   {
@@ -62,6 +80,9 @@ const products = [
     description:
       "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
     category: "jewelery",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
   },
   {
@@ -71,6 +92,9 @@ const products = [
     description:
       "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
     category: "jewelery",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
   },
   {
@@ -80,6 +104,9 @@ const products = [
     description:
       "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
   },
   {
@@ -89,6 +116,9 @@ const products = [
     description:
       "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
   },
   {
@@ -99,6 +129,9 @@ const products = [
     description:
       "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
   },
   {
@@ -109,6 +142,9 @@ const products = [
     description:
       "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
   },
   {
@@ -118,6 +154,9 @@ const products = [
     description:
       "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
   },
   {
@@ -128,6 +167,9 @@ const products = [
     description:
       "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
     category: "electronics",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
   },
   {
@@ -137,6 +179,9 @@ const products = [
     description:
       "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
   },
   {
@@ -147,6 +192,9 @@ const products = [
     description:
       "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
   },
   {
@@ -156,6 +204,9 @@ const products = [
     description:
       "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
   },
   {
@@ -165,6 +216,9 @@ const products = [
     description:
       "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
   },
   {
@@ -174,6 +228,9 @@ const products = [
     description:
       "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
   },
   {
@@ -183,6 +240,9 @@ const products = [
     description:
       "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
     category: "women clothing",
+    addedToCart: false,
+    addToCartButtonValue: "Add to Cart",
+    addToCartButtonClass: "btn btn-info",
     image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
   },
 ];
