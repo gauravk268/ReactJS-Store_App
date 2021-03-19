a04b1f3d1559691c6e4f19d832e04f7c9bd9bec5
Gaurav Kumar <gauravk26800@gmail.com>
2021-03-19 23:42:15 +0530

    fixed items api down problem

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import {
 } from "./components/";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import db from './components/firebase/config';
-import products from "./components/data/products";
+// import products from "./components/data/products";   // temporary method while api is down
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
-  const [items, setItems] = useState([...products]);
+  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState([...products]); // temporary method while api is down
 
   useEffect(() => {
     const getLocalCart = () => {
@@ -37,26 +38,29 @@ function App() {
     saveLocalCart();
   }, [cartItems]);
 
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     const response = await fetch("https://fakestoreapi.com/products");
-  //     const data = await response.json();
-  //     console.log(data);
-  //     setItems(
-  //       data.map((item) => {
-  //         return {
-  //           ...item,
-  //           addedToCart: false,
-  //           addToCartButtonValue: "Add to Cart",
-  //           addToCartButtonClass: "btn btn-info",
-  //         };
-  //       })
-  //     );
-  //     // console.log(data);
-  //   };
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(
+        "https://fakestoreapi.herokuapp.com/products"
+      );
+      const data = await response.json();
+      console.log(data);
+      setItems(
+        data.map((item) => {
+          return {
+            ...item,
+            image: item.image.replace(/fakestoreapi/, "fakestoreapi.herokuapp"), // temporary method while api is down
+            addedToCart: false,
+            addToCartButtonValue: "Add to Cart",
+            addToCartButtonClass: "btn btn-info",
+          };
+        })
+      );
+      // console.log(data);
+    };
 
-  //   getProducts();
-  // }, []);
+    getProducts();
+  }, []);
 
   return (
     <Router>
